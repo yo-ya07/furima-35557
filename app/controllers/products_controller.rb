696c8d24f825282ab746fee89ea495c1efbd7b51
yo-ya07 <doro.fomano101@gmail.com>
2021-06-05 17:04:51 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order(id: "DESC")
@@ -19,17 +20,19 @@ class ProductsController < ApplicationController
   end
   
   def show
-    @product = Product.find(params[:id])
   end
 
+  def edit
+    redirect_to root_path unless current_user.id == @product.user_id
+  end
 
-  # def edit
-  #   @product  = Product.new
-  # end
-
-  # def update
-  #   @product  = Product.new
-  # end
+  def update
+    if @product.update(product_params)
+        redirect_to products_path
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   @product  = Product.new
@@ -38,5 +41,9 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:image, :name, :info, :category_id, :status_id, :postage_id, :region_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
